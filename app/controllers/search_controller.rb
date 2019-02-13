60fbace403dcafc8ed7f24215deddd4a4ce0bfc7
Angela Unrau <angie_tora@hotmail.com>
2019-02-13 16:47:08 -0600

    Worked on the beginnings of the Search results page output.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
   def results
+    @query = params[:q]
+    @guides_found = Guide.where('name LIKE ?', "%#{@query}%")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'search/results'
   resources :planets, only: [:index, :show]
   resources :guides
 
+  get 'search_results', to: 'search#results', as: 'search_results'
+
   root to: 'guides#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
